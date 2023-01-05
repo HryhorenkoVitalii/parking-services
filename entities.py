942592ddb6e985624e47a78c8f83cadac69d24ff
Hryhorenko Vitalii <hryhorenko.vitalii@gmail.com>
2023-01-04 23:41:57 -0500

    pretty format entities

--- entities.py
+++ entities.py
@@ -1,19 +1,19 @@
-from typing import NamedTuple
 from enum import Enum
+from typing import NamedTuple
+
 
 class Permissions(Enum):
     vip_permission = "vip_permission"
     permission1 = "permission1"
     permission2 = "permission2"
     permission3 = "permission3"
-    
-    
+
     @classmethod
     def has_value(cls, value):
-        return value in cls._value2member_map_ 
+        return value in cls._value2member_map_
 
 
 class PermissionPriorities(NamedTuple):
-    first_priorities: list(Permissions)
-    second_priorities: list(Permissions)
-    thirst_priorities: list(Permissions) 
+    first_priorities: list[Permissions]
+    second_priorities: list[Permissions]
+    thirst_priorities: list[Permissions]
