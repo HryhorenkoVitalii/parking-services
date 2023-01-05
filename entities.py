7fee735fd887c59e7873ada1bdba5d6fda052395
Hryhorenko Vitalii <hryhorenko.vitalii@gmail.com>
2023-01-04 23:35:34 -0500

    fix name and add has_value method

--- entities.py
+++ entities.py
@@ -2,10 +2,15 @@ from typing import NamedTuple
 from enum import Enum
 
 class Permissions(Enum):
-    vipv_permission = "vip_permission"
+    vip_permission = "vip_permission"
     permission1 = "permission1"
     permission2 = "permission2"
     permission3 = "permission3"
+    
+    
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_ 
 
 
 class PermissionPriorities(NamedTuple):
